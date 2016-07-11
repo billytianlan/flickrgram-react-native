77cd5d03d315c703ff8203af47166286e4cd03d1
Billy Lan <billylan22@gmail.com>
2016-07-11 13:01:24 -0700

    basic display image on screen

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+'use strict';
 
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
   View
 } from 'react-native';
 
 class flickrgramReactNative extends Component {
+
+  constructor(props) { 
+    super(props);
+    this.state = {
+      photos: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/photos')
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        console.log(responseJSON);
+        this.setState({
+          photos: responseJSON
+        })
+      })
+      .catch((err) => {
+        throw(err);
+      });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        {this.state.photos.map((photo) => {
+          return (
+            <Image style={styles.photo} key={photo.id} source={{uri: photo.url}}/>
+          )
+        })}
       </View>
     );
   }
@@ -48,6 +62,11 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  photo: {
+    flex: 1,
+    width: 400, 
+    height: 400
+  }
 });
 
 AppRegistry.registerComponent('flickrgramReactNative', () => flickrgramReactNative);
